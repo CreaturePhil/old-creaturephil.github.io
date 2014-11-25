60ea2a25a747e9f68a6caabbb61488b33f057276
Phil <birkal@outlook.com>
2014-11-25 00:04:45 -0800

    Add Grunt and gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    jade: {
+      files: {
+        'index.html': 'src/index.jade'
+      }
+    },
+
+    stylus: {
+      compile: {
+        files: {
+          'css/main.css': 'stylus/main.styl'
+        }
+      }
+    },
+
+    watch: {
+      jade: {
+        files: 'src/*.jade',
+        tasks: ['jade']
+      },
+      stylus: {
+        files: 'stylus/*.styl',
+        tasks: ['stylus']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('html', ['jade']);
+  grunt.registerTask('css', ['stylus']);
+
+  grunt.registerTask('default', ['jade', 'stylus', 'watch']);
+
+};
